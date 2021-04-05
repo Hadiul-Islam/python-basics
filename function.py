0667f6b1cc3fdc9a02a7b51d1f075ab7b9a56355
hadiuldev <hadiulislamofficial@gmail.com>
2021-04-06 00:37:44 +0600

    add hello and function

---
+++ function.py
@@ -0,0 +1,8 @@
+def add_two_values(first, second):
+    return first + second
+
+num_1 = 130
+num_2 = 130
+sum = add_two_values(num_1, num_2)
+
+print(f'{num_1} + {num_2} = {sum}')
---
+++ hello.py
@@ -0,0 +1,6 @@
+# #print
+print('first time with python')
+# #input
+username = input('write your name: ')
+print(f'hello {username}')
+
