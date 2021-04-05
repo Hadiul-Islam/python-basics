9b0266c29ed299bcc0899ef71e0cbb31f9728aa1
hadiuldev <hadiulislamofficial@gmail.com>
2021-04-06 01:36:21 +0600

    add str and num

---
+++ data-type.py
@@ -0,0 +1,28 @@
+## String and Number with use 
+
+first_num = 33
+second_num = 10
+
+addition = first_num + second_num 
+subtraction = first_num - second_num
+division = first_num / second_num
+quotient = first_num // second_num
+remainder = first_num % second_num
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Division: {division}")
+print(f"quotient: {quotient}")
+print(f"Remainder: {remainder}")
+
+
+user_name = "hadiul"
+country = "Bangladesh"
+age = 20
+age_value = str(age)
+
+print(f"Name : {user_name}  Country: {country}  Age: {age_value}")
+print(len(user_name))
+
+# List 
+
