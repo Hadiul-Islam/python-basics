b1552a439ad7b4b1b86bde38dfaa520a39e3b1b5
hadiuldev <hadiulislamofficial@gmail.com>
2021-04-12 16:38:35 +0600

    add list

--- list.py
+++ list.py
@@ -2,4 +2,8 @@ lang_list = ["C", "python", "javascript", "java"]
 print(lang_list)
 lang_list.append("C++") # append use for add new item
 print(lang_list)
-print(lang_list[2])
+lang_list.remove("java") # remove use for remove items
+print(lang_list)
+
+
+
