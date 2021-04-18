15e0df440a8da12dce4944908c56f6f2b2d8f415
hadiuldev <hadiulislamofficial@gmail.com>
2021-04-19 04:31:02 +0600

    add conditional.py

---
+++ conditional/ifElse.py
@@ -0,0 +1,23 @@
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+# example 2
+
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# example 3
+
+age = input("Your age: ")
+ageNum =int(age)
+
+if ageNum >= 18:
+    print("You are adult")
+else:
+    print("You are baby")
