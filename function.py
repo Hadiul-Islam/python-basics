4c66356e14b959149e8333118374831959c36036
hadiuldev <hadiulislamofficial@gmail.com>
2021-04-19 13:19:15 +0600

    add data type

--- function.py
+++
@@ -1,8 +0,0 @@
-def add_two_values(first, second):
-    return first + second
-
-num_1 = 130
-num_2 = 130
-sum = add_two_values(num_1, num_2)
-
-print(f'{num_1} + {num_2} = {sum}')
--- function/function.py
+++ function/function.py
@@ -2,5 +2,9 @@ def add(x,y,z):
     sum = x + y + z
     print(sum)
 
+def sub(x,y):
+    sum = x - y 
+    print(sum)
+
 add(2,55 , 66)
-add(255, 5,5)
+sub(255, 5)
