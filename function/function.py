7fb68efb56064a2041c51c8209bbd735c992cb15
hadiul-islam <hadiulislamofficial@gmail.com>
2022-07-20 00:36:05 +0600

    binary search function

--- function/function.py
+++ function/function.py
@@ -1,3 +1,6 @@
+from tracemalloc import start
+
+
 def add(x,y,z):
     sum = x + y + z
     print(sum)
@@ -6,5 +9,27 @@ def sub(x,y):
     sum = x - y 
     print(sum)
 
+
+#binary search
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+
+
+
 add(2,55 , 66)
 sub(255, 5)
+print(binary_search([1,3,5,7,9], 9))
